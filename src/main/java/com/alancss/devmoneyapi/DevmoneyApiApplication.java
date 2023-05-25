1d6c198a4b223e2f201627709aeff61631351e78
Alan Carlos <alanclash2541@gmail.com>
2023-05-24 23:09:28 -0300

    Project created

---
+++ src/main/java/com/alancss/devmoneyapi/DevmoneyApiApplication.java
@@ -0,0 +1,13 @@
+package com.alancss.devmoneyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevmoneyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevmoneyApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alancss/devmoneyapi/DevmoneyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alancss.devmoneyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevmoneyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
